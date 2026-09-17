f4b92a20be3204cdfaf76227e50e7b249c690209
MARIA LUIZA APARECIDA SALES MANZANO <26171901@edu119.local>
2026-09-17 09:52:03 -0300

    atualizando

---
+++ servidor.js
@@ -0,0 +1,21 @@
+const express  = require("express");
+
+const app = express();
+
+let ALUNOS = [
+    {id:1, nome: "Bernardo", curso: "Desenvolvimento de sistemas"},
+    {id:2, nome: "Camily", curso: "Redes de computadores"},
+    {id:3, nome: "Kaue", curso: "Banco de dados"},
+    {id: 4, nome: "Maria", curso:"Administração"},
+    {id: 5, nome: "Marjory", curso:"Desenvolvimento de sistemas "},
+]
+app.get("/",(req,res) =>{
+res.json({
+message:"API Alunos funcionando"
+  })
+})
+const PORTA = 3000;
+app.listen(PORTA, ()=>{
+    console.log("Servidor inicado com sucesso");
+    console.log(`https://localhost:${PORTA}`);
+})
